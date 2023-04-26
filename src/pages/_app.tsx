df88c35aed7329b5afe3d477508ec89de9576f48
Zack Hanley <110628956+zaHanley@users.noreply.github.com>
2023-04-26 14:43:34 -0400

    Initial commit with Prisma schema

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { type AppType } from "next/app";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
